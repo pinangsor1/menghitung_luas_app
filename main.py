0cc9e99d710dc3e9cd89854633eb8aa9e61f843f
Yesa <79618650+pinangsor1@users.noreply.github.com>
2022-10-28 13:55:55 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.write("""
+# aplikasi menghitung luas sederhana 
+""")
+
+alas = st.write("masukkan alas = ", 0)
+tinggi = st.write("masukkan tinggi = ", 0)
+luas = st.button("luas")
+
+if hitung:
+  
